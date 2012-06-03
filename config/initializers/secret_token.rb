ae6e38ab694166def84b5e526666abed240e9b86
Jean-Nicholas <nick.hould@gmail.com>
2012-06-03 13:49:22 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webjobs::Application.config.secret_token = 'ea7e41e34d0a175bf1795ecc4f8ab4e194a98664b3cf2f69efa3f36c5318504fecd56b6fa0b4342d62702ca95d677aced117bf6cb577d36c92aa605af01c8a1b'
