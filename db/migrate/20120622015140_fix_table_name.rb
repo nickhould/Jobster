d4fedcd034933831d6f7c81585c0ec36145ec82e
Jean-Nicholas <nick.hould@gmail.com>
2012-06-21 22:17:39 -0400

    Remove unsollicited migrations

--- db/migrate/20120622015140_fix_table_name.rb
+++
@@ -1,9 +0,0 @@
-class FixTableName < ActiveRecord::Migration
-  def self.up
-    rename_table :scrapers, :sources
-  end
-
- def self.down
-    rename_table :sources, :scrapers
- end
-end
--- db/migrate/20120622015354_fix_job_sources_table_name.rb
+++
@@ -1,9 +0,0 @@
-class FixJobSourcesTableName < ActiveRecord::Migration
-  def up
-  	rename_table :job_scrapers, :job_sources
-  end
-
-  def down
-    rename_table :job_sources, :job_scrapers
-  end
-end
